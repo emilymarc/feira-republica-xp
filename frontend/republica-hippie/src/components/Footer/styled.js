d598f8fc43564641de9b5fb098fc22a3a539150c
Manu <manuelasbela@gmail.com>
2022-06-29 19:29:44 -0300

    criando footer

--- frontend/republica-hippie/src/components/Footer/styled.js
+++ frontend/republica-hippie/src/components/Footer/styled.js
@@ -1,1 +1,8 @@
-import styled from "styled-components"
+import styled from "styled-components";
+
+export const Footer = styled.footer`
+    display: flex;
+    justify-content: space-between;
+    background-color: #746A5C;
+    color: #FFFF;
+`
