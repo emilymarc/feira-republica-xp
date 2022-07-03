ee8bcc854f4a14e4a03a1318c1cd3d8b982b0527
Geovane Silva <geovanesilva.nar@hotmail.com>
2022-07-03 11:37:12 -0300

    Seeders Exhibitors

--- backend/src/infrastructure/database/seeders/20220626105101-exhibitors.js
+++ backend/src/infrastructure/database/seeders/20220626105101-exhibitors.js
@@ -71,7 +71,21 @@ module.exports = {
       phone: '(11) 91234-5678',
       phrase: faker.lorem.lines(1),
       description: faker.lorem.lines(6),
-      img: 'https://res.cloudinary.com/dwb977d8l/image/upload/v1656554512/Imagens-Chefao/Rectangle_132_pgs7dw.png',
+      img: 'https://res.cloudinary.com/dwb977d8l/image/upload/v1656854902/Imagens-Chefao/imagem_materia_arqb0l.jpg',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      data_status: 1
+    },
+      {
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      password: '$2a$10$/LY08X0AvQfRdznlMn1yn.OBdbWVNZ7QPC7IG2Gkjyz.ToOnqS6TO',
+      profession: faker.lorem.lines(1),
+      birth_date: new Date(),
+      phone: '(11) 91234-5678',
+      phrase: faker.lorem.lines(1),
+      description: faker.lorem.lines(6),
+      img: 'https://res.cloudinary.com/dwb977d8l/image/upload/v1656854902/Imagens-Chefao/artesao-trabalhando-730x480_dxba5d.jpg',
       createdAt: new Date(),
       updatedAt: new Date(),
       data_status: 1
@@ -90,6 +104,7 @@ module.exports = {
       updatedAt: new Date(),
       data_status: 1
     },
+
       {
       name: faker.name.findName(),
       email: faker.internet.email(),
@@ -99,7 +114,49 @@ module.exports = {
       phone: '(11) 91234-5678',
       phrase: faker.lorem.lines(1),
       description: faker.lorem.lines(6),
-      img: 'https://res.cloudinary.com/dwb977d8l/image/upload/v1656554512/Imagens-Chefao/Rectangle_119_ewte0g.png',
+      img: 'https://res.cloudinary.com/dwb977d8l/image/upload/v1656854786/Imagens-Chefao/artes%C3%A3o_1034771065_ep5m8p.jpg',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      data_status: 1
+    },
+      {
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      password: '$2a$10$/LY08X0AvQfRdznlMn1yn.OBdbWVNZ7QPC7IG2Gkjyz.ToOnqS6TO',
+      profession: faker.lorem.lines(1),
+      birth_date: new Date(),
+      phone: '(11) 91234-5678',
+      phrase: faker.lorem.lines(1),
+      description: faker.lorem.lines(6),
+      img: 'https://res.cloudinary.com/dwb977d8l/image/upload/v1656854785/Imagens-Chefao/transferir_1_bfldcy.jpg',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      data_status: 1
+    },
+      {
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      password: '$2a$10$/LY08X0AvQfRdznlMn1yn.OBdbWVNZ7QPC7IG2Gkjyz.ToOnqS6TO',
+      profession: faker.lorem.lines(1),
+      birth_date: new Date(),
+      phone: '(11) 91234-5678',
+      phrase: faker.lorem.lines(1),
+      description: faker.lorem.lines(6),
+      img: 'https://res.cloudinary.com/dwb977d8l/image/upload/v1656854785/Imagens-Chefao/transferir_o9i6yu.jpg',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      data_status: 1
+    },
+      {
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      password: '$2a$10$/LY08X0AvQfRdznlMn1yn.OBdbWVNZ7QPC7IG2Gkjyz.ToOnqS6TO',
+      profession: faker.lorem.lines(1),
+      birth_date: new Date(),
+      phone: '(11) 91234-5678',
+      phrase: faker.lorem.lines(1),
+      description: faker.lorem.lines(6),
+      img: 'https://res.cloudinary.com/dwb977d8l/image/upload/v1656854785/Imagens-Chefao/images_oszfht.jpg',
       createdAt: new Date(),
       updatedAt: new Date(),
       data_status: 1
