592b9e87c04dd85c739e26665b25cd613cbeaac6
Victor Hugo Feracin <victorhugo.feracin@gmail.com>
2022-07-03 12:57:27 -0300

    Adicionando estilização de página ativa no header

--- frontend/republica-hippie/src/components/Header/index.jsx
+++ frontend/republica-hippie/src/components/Header/index.jsx
@@ -4,11 +4,13 @@ import search from '../../assets/Icon.svg';
 import cart from '../../assets/shopping-cart.svg';
 import profile from '../../assets/profile.svg';
 import iconExpositores from '../../assets/IconExpositores.svg';
+import iconExpositoresActive from '../../assets/IconExpositoresActive.svg';
 import * as S from './styled';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
-
+    const fullUrl = window.location.href
+    const compareUrl = `http://localhost:3000`
     return (
         <S.Header>
             <S.LogoArea>
@@ -30,13 +32,13 @@ const Header = () => {
 
             <S.NavArea>
                     <S.Nav>
-                        <S.NavLink to='/'><S.NavItem>HOME</S.NavItem></S.NavLink>
-                        <S.NavLink to='/ceramicas'><S.NavItem>CERÂMICAS</S.NavItem></S.NavLink>
-                        <S.NavLink to='/colares'><S.NavItem>COLARES</S.NavItem></S.NavLink>
-                        <S.NavLink to='/pinturas'><S.NavItem>PINTURAS</S.NavItem></S.NavLink>
+                        {fullUrl == `${compareUrl}/` ? <S.NavLink to='/'><S.NavItem style={{color: '#98D2D3', fontWeight: '600'}}>HOME</S.NavItem></S.NavLink> : <S.NavLink to='/'><S.NavItem>HOME</S.NavItem></S.NavLink>}
+                        {fullUrl == `${compareUrl}/ceramicas` ? <S.NavLink to='/ceramicas'><S.NavItem style={{color: '#98D2D3', fontWeight: '600'}}>CERÂMICAS</S.NavItem></S.NavLink> : <S.NavLink to='/ceramicas'><S.NavItem>CERÂMICAS</S.NavItem></S.NavLink>}
+                        {fullUrl == `${compareUrl}/colares` ? <S.NavLink to='/colares'><S.NavItem style={{color: '#98D2D3', fontWeight: '600'}}>COLARES</S.NavItem></S.NavLink> : <S.NavLink to='/colares'><S.NavItem>COLARES</S.NavItem></S.NavLink>}
+                        {fullUrl == `${compareUrl}/pinturas` ? <S.NavLink to='/pinturas'><S.NavItem style={{color: '#98D2D3', fontWeight: '600'}}>PINTURAS</S.NavItem></S.NavLink> : <S.NavLink to='/pinturas'><S.NavItem>PINTURAS</S.NavItem></S.NavLink>}
                         <S.ContainerExpositores>
-                            <img src={iconExpositores} alt='Icon Expositores'></img>
-                            <S.NavLink to='/expositores'><S.NavItem>EXPOSITORES</S.NavItem></S.NavLink>
+                            {fullUrl == `${compareUrl}/expositores` ? <img src={iconExpositoresActive} alt='Icon Expositores'></img> : <img src={iconExpositores} alt='Icon Expositores'></img>}
+                            {fullUrl == `${compareUrl}/expositores` ? <S.NavLink to='/expositores'><S.NavItem style={{color: '#98D2D3', fontWeight: '600'}}>EXPOSITORES</S.NavItem></S.NavLink> : <S.NavLink to='/expositores'><S.NavItem>EXPOSITORES</S.NavItem></S.NavLink>}
                         </S.ContainerExpositores>
                     </S.Nav>
             </S.NavArea>
