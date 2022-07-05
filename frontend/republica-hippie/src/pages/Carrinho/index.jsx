5335a99d27aa1139685d29095147ce539874d888
Victor Hugo Feracin <victorhugo.feracin@gmail.com>
2022-07-05 05:14:22 -0300

    Adicionando e estilizando página de carrinho

--- frontend/republica-hippie/src/pages/Carrinho/index.jsx
+++ frontend/republica-hippie/src/pages/Carrinho/index.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import DetailedBg from '../../components/DetailedBg';
+import CarrinhoComponent from '../../components/CarrinhoComponent';
 const Artesaos = () => {
   return ( 
     <>
         <Header></Header>
           <DetailedBg>
-            
+            <CarrinhoComponent></CarrinhoComponent>
           </DetailedBg>
         <Footer></Footer>
     </>
