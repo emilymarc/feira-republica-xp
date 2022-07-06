23b9788b584a8e7bf88ebc8904d676d5af0b71ac
Victor Hugo Feracin <victorhugo.feracin@gmail.com>
2022-07-06 18:59:28 -0300

    Expositores

--- frontend/republica-hippie/src/components/ExpositoresList/index.jsx
+++ frontend/republica-hippie/src/components/ExpositoresList/index.jsx
@@ -1,50 +1,44 @@
-import React, { useState, useEffect } from 'react';
-import * as S from './styled';
-import foto from '../../assets/Rectangle 119 (1).svg';
-import {Link} from 'react-router-dom';
-import { getExpositores } from '../../services/api';
-import { toast } from 'react-toastify';
+import React, { useState, useEffect } from "react";
+import * as S from "./styled";
+import foto from "../../assets/Rectangle 130.svg";
+import { baseUrl, getExpositores } from "../../services/api";
+import { toast } from "react-toastify";
 
 const ExpositoresList = () => {
+  const { expositores, setExpositores } = useState([]);
 
-    const {expositores, setExpositores} = useState({}) 
-    useEffect (() => {
-        const loadExpositores = async () => {
-            try {
-                const response = await getExpositores()
-                setExpositores(response.data) 
-                console.log(setExpositores);
-            } catch (error) {
-                toast.error(`Erro ao carregar expositores ${error}`)
-            }
-        }
-        loadExpositores()
-    }, [setExpositores])
+  useEffect(() => {
+    const loadExpositores = async () => {
+      try {
+        const response = await getExpositores();
+        setExpositores(response.data);
+      } catch (error) {
+        toast.warn(`Erro ao carregar expositores`);
+      }
+    };
+    loadExpositores();
+  }, [setExpositores]);
 
-    return (
-        expositores.map((expositor) => {
-            <S.ExpositoresContainer>
-    
-            <S.ImgContainer>
-                <img src={foto} alt="Expositor"/>
-            </S.ImgContainer>
-    
-            <S.InfoExpositores>
-                <S.TitleExpositores>expositores</S.TitleExpositores>
-                <p>
-                    Pintor - Pintura em azulejos <br/>
-                    76 anos
-                </p>
-                <p>
-                    Contato: (11) 91234-5678  <br/>
-                    <S.LinkInfo to="/">
-                       Mais sobre José Carlos
-                    </S.LinkInfo>
-                </p>
-            </S.InfoExpositores>
-        </S.ExpositoresContainer>
-        }) 
-    )
-}
+  console.log(`Expositores ${expositores}`);
+  return (
+    <S.ExpositoresContainer>
+      <S.ImgContainer>
+        <img src={foto} alt="Expositor" />
+      </S.ImgContainer>
+
+      <S.InfoExpositores>
+        <S.TitleExpositores>expositores</S.TitleExpositores>
+        <p>
+          Pintor - Pintura em azulejos <br />
+          76 anos
+        </p>
+        <p>
+          Contato: (11) 91234-5678 <br />
+          <S.LinkInfo to="/">Mais sobre José Carlos</S.LinkInfo>
+        </p>
+      </S.InfoExpositores>
+    </S.ExpositoresContainer>
+  );
+};
 
 export default ExpositoresList;
--- frontend/republica-hippie/src/pages/Ceramica/index.jsx
+++ frontend/republica-hippie/src/pages/Ceramica/index.jsx
@@ -62,3 +62,4 @@ const Ceramica = () => {
 };
 
 export default Ceramica;
+
