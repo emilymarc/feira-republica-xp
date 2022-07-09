46ca1a4f1a7d5d966ac5708f63a1dc71bcbd88d0
Victor Hugo Feracin <victorhugo.feracin@gmail.com>
2022-07-08 22:08:28 -0300

    Estilizando página do expositor

--- frontend/republica-hippie/src/components/ExpositorComponent/styled.js
+++ frontend/republica-hippie/src/components/ExpositorComponent/styled.js
@@ -7,3 +7,30 @@ export const Container = styled.div`
 export const ProductsContainer = styled.div`
 
 `
+
+export const ExpositorTitle = styled.h1`
+  font-size: 50px;
+  color: #C57E5F;
+  margin: 40px auto;
+`
+
+export const ExpositorCite = styled.cite`
+  margin: 30px auto auto auto;
+  font-style: normal;
+  display: block;
+`
+
+export const ExpositorImg = styled.img`
+  margin: 45px auto;
+`
+
+export const ExpositorDescription = styled.p`
+  max-width: 1000px;
+  margin: auto auto 45px auto;
+`
+
+export const ExpositorProductTitle = styled.h3`
+  font-size: 28px;
+  font-weight: 700;
+  margin-bottom: 25px
+`
