e3e00fe98134bc0ebd178fe44c887b8c75512118
Emily Marcolino <emily.marcolino@gmail.com>
2022-07-10 19:23:33 -0300

    add backend url from heroku deploy

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify'
 
 export const baseUrl = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://feira-republica.herokuapp.com",
 })
 
 //Products
